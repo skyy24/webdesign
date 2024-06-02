fba642d07d8b299d248b9146c784ed7c54edfc98
skyy24 <165016248+skyy24@users.noreply.github.com>
2024-06-02 09:57:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function play(){
             }else{
                 if(bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width && bird_props.left + bird_props.width > pipe_sprite_props.left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height && bird_props.top + bird_props.height > pipe_sprite_props.top){
                     game_state = 'End';
-                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Pencet ENTER Untuk Restart';
+                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Kalo mau ngulang pencet ENTER!!!';
                     message.classList.add('messageStyle');
                     img.style.display = 'none';
                     sound_die.play();
